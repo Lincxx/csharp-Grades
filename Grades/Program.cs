fb2b6bc9f5dd7586abf152a14148dfaed0b49547
Jeremy Lincoln <bossco28@gmail.com>
2017-05-09 14:58:53 -0400

    File writing

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Synthesis;
+using System.IO;
 
 namespace Grades
 {
@@ -11,12 +12,53 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            
+
             //SpeechSynthesizer synth = new SpeechSynthesizer();
             //synth.Speak("Hello, this is the grade book program");
 
             GradeBook book = new GradeBook();
 
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+
+            //synth.Speak(nameof(stats.AverageGrade ) + " " + stats.AverageGrade);
+            //synth.Speak(nameof(stats.HighestGrade) + " " + stats.HighestGrade);
+            //synth.Speak(nameof(stats.LowestGrade) + " " + stats.LowestGrade);
+
+            //Console.WriteLine(nameof(stats.AverageGrade) + " " + stats.AverageGrade);
+            //Console.WriteLine(nameof(stats.HighestGrade) + " " + stats.HighestGrade);
+            //Console.WriteLine(nameof(stats.LowestGrade) + " " + stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("HighestGrade", (int)stats.HighestGrade);
+            WriteResult("LowestGrade", stats.LowestGrade);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75f);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
             try
             {
                 Console.WriteLine("Enter a name");
@@ -34,29 +76,7 @@ namespace Grades
             {
                 Console.WriteLine(ex.Message);
             }
-
-
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75f);
-            book.WriteGrades(Console.Out);
-
-            GradeStatistics stats =  book.ComputeStatistics();
-
-            //synth.Speak(nameof(stats.AverageGrade ) + " " + stats.AverageGrade);
-            //synth.Speak(nameof(stats.HighestGrade) + " " + stats.HighestGrade);
-            //synth.Speak(nameof(stats.LowestGrade) + " " + stats.LowestGrade);
-
-            //Console.WriteLine(nameof(stats.AverageGrade) + " " + stats.AverageGrade);
-            //Console.WriteLine(nameof(stats.HighestGrade) + " " + stats.HighestGrade);
-            //Console.WriteLine(nameof(stats.LowestGrade) + " " + stats.LowestGrade);
-            Console.WriteLine(book.Name);
-            WriteResult("Average", stats.AverageGrade);
-            WriteResult("HighestGrade", (int)stats.HighestGrade);
-            WriteResult("LowestGrade", stats.LowestGrade);
-            WriteResult(stats.Description, stats.LetterGrade);
         }
-
 
         static void WriteResult(string description, string result)
         {
